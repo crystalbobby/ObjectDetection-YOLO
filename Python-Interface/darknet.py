175e5a30e47f3358e83343deeb6c7d89a682b5c5
latermonk <iback.china@gmail.com>
2019-11-18 11:57:21 +0800

    Update darknet.py

--- Python-Interface/darknet.py
+++ Python-Interface/darknet.py
@@ -143,14 +143,14 @@ def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
     return res
     
 if __name__ == "__main__":
-    #net = load_net("cfg/densenet201.cfg", "/home/pjreddie/trained/densenet201.weights", 0)
+    #net = load_net("cfg/yolov3-tiny.cfg", "/root/src/darknet/yolov3-tiny.weights", 0)
     #im = load_image("data/wolf.jpg", 0, 0)
     #meta = load_meta("cfg/imagenet1k.data")
     #r = classify(net, meta, im)
     #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
+    net = load_net("cfg/yolov3-tiny.cfg", "yolov3-tiny.weights", 0)
     meta = load_meta("cfg/coco.data")
     r = detect(net, meta, "data/dog.jpg")
-    print r
+    print (r)
     
 
